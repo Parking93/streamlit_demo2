a0c662dcdaecfd42938137e6b8109a665b1eb23f
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2024-06-05 14:28:54 +0900

    Update demopage2.py

--- demopage2.py
+++ demopage2.py
@@ -1,13 +1,12 @@
 import streamlit as st
-import os
 
-st.set_page_config(initial_sidebar_state="collapsed")
+# st.set_page_config(initial_sidebar_state="collapsed")
 
-st.page_link("demopage.py", label="Home", icon="🏠")
+# st.page_link("demopage.py", label="Home", icon="🏠")
 
-st.page_link("pages/untitled1.py", label="untitled1", icon="1️⃣")
+# st.page_link("pages/untitled1.py", label="untitled1", icon="1️⃣")
 
-st.page_link("pages/stockdashboard.py", label="stockdashboard", icon="📈")
+# st.page_link("pages/stockdashboard.py", label="stockdashboard", icon="📈")
 
-st.page_link("https://titanic-dashboard.streamlit.app/", label="Google", icon="🌎")
+# st.page_link("https://titanic-dashboard.streamlit.app/", label="Google", icon="🌎")
 
