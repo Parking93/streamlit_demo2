0d23b6f2e8954e57a4576dc1b7aecfbed0e7d79d
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2024-06-05 14:22:15 +0900

    Add files via upload

---
+++ demopage.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import os
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+st.page_link("demopage.py", label="Home", icon="🏠")
+
+st.page_link("pages/untitled1.py", label="untitled1", icon="1️⃣")
+
+st.page_link("pages/stockdashboard.py", label="stockdashboard", icon="📈")
+
+st.page_link("https://titanic-dashboard.streamlit.app/", label="Google", icon="🌎")
+
---
+++ untitled1.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.page_link("demopage.py", label="Home", icon="🏠")
